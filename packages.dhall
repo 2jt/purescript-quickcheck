4aab105c2eac4a088561bbfd8a1229b861974035
jtomas <me@jtomas.com>
2022-03-21 13:22:47 +0100

    es modules

---
+++ packages.dhall
@@ -0,0 +1,5 @@
+let upstream =
+      https://raw.githubusercontent.com/2jt/package-sets/main/packages.dhall
+        sha256:cbf5479e6813e4d9b4eb1083a4521988f48e790943c54181598ace393c377670
+
+in  upstream
--- src/Test/QuickCheck/Gen.js
+++ src/Test/QuickCheck/Gen.js
@@ -1,12 +1,9 @@
 /* global ArrayBuffer, Float32Array, Int32Array */
-"use strict";
 
-// module Test.QuickCheck.Gen
-
-exports.float32ToInt32 = function (n) {
-  var arr = new ArrayBuffer(4);
-  var fv = new Float32Array(arr);
-  var iv = new Int32Array(arr);
+export function float32ToInt32(n) {
+  const arr = new ArrayBuffer(4);
+  const fv = new Float32Array(arr);
+  const iv = new Int32Array(arr);
   fv[0] = n;
   return iv[0];
-};
+}
